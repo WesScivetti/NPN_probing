b206170c49ec525ebd1553eb8f87fa5f1855c528
WesScivetti <wesleyscivetti@my.unt.edu>
2025-03-15 01:38:23 -0400

    update outputs

--- run_few_shots.sh
+++ run_few_shots.sh
@@ -45,39 +45,39 @@
 #python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --semantic --split_seed 9
 #python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --semantic --split_seed 10
 #next were the last run
-python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 10 --split_seed 1
-python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 10 --split_seed 2
-python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 10 --split_seed 3
-python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 10 --split_seed 4
-python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 10 --split_seed 5
+#python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 10 --split_seed 1
+#python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 10 --split_seed 2
+#python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 10 --split_seed 3
+#python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 10 --split_seed 4
+#python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 10 --split_seed 5
 #python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 10 --split_seed 6
 #python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 10 --split_seed 7
 #python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 10 --split_seed 8
 #python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 10 --split_seed 9
 #python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 10 --split_seed 10
 #got through 1
-python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 25 --split_seed 1
-python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 25 --split_seed 2
-python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 25 --split_seed 3
-python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 25 --split_seed 4
-python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 25 --split_seed 5
-#python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 25 --split_seed 6
-#python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 25 --split_seed 7
-#python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 25 --split_seed 8
-#python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 25 --split_seed 9
-#python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 25 --split_seed 10
-#
-python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 100 --split_seed 1
-python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 100 --split_seed 2
-python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 100 --split_seed 3
-python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 100 --split_seed 4
-python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 100 --split_seed 5
+#python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 25 --split_seed 1
+#python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 25 --split_seed 2
+#python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 25 --split_seed 3
+#python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 25 --split_seed 4
+#python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 25 --split_seed 5
+##python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 25 --split_seed 6
+##python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 25 --split_seed 7
+##python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 25 --split_seed 8
+##python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 25 --split_seed 9
+##python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 25 --split_seed 10
+##
+#python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 100 --split_seed 1
+#python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 100 --split_seed 2
+#python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 100 --split_seed 3
+#python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 100 --split_seed 4
+#python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 100 --split_seed 5
 #python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 100 --split_seed 6
 #python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 100 --split_seed 7
 #python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 100 --split_seed 8
 #python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 100 --split_seed 9
 #python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --num_train 100 --split_seed 10
-#
+#NEED TO RUN THESE AT THE END
 python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --split_seed 1
 python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --split_seed 2
 python3 get_embeddings.py -d ./data/Kat_clean6.tsv -i ./data/train_test_split_train_balanced_Y_big_yTest_Kat_Clean.json --split_seed 3
